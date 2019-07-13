ab277a5d8712a25af1eedc1dda1fe28f80488338
brandonroesler <roesler19@gmail.com>
2019-07-12 19:11:34 -0500

    Updates

---
+++ precache-manifest.bd2158c50c8a1ebf8e02f5d6968bf54b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c000ea8da11c5edc674aeb6c9e213a",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/index.html"
+  },
+  {
+    "revision": "cb1306e890fb7856912b",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/css/main.c6533eb9.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cb1306e890fb7856912b",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/main.e5ccaeb2.chunk.js"
+  },
+  {
+    "revision": "d5f08aa8dc1074a3a93f",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/runtime~main.51c75cd6.js"
+  }
+]);
